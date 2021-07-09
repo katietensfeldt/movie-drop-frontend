20adb77d203be96df826d69fa4d8853854350c31
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-07-09 16:30:01 -0700

    Adds styles to movie index and show

--- src/views/movies/Index.vue
+++ src/views/movies/Index.vue
@@ -1,144 +1,74 @@
 <template>
   <div class="movies-index">
-    <div class="contain-wrapp padding-clear padding-bottom-30">
-      <!-- START - Gallery Filter  -->
-      <ul class="filter-items filter-wrapp">
-        <form v-on:submit.prevent="findMovie()">
-          <li>
-            <input class="form-control" type="text" v-model="search" placeholder="Search by movie title..." />
-          </li>
-          <li>
-            <input class="btn-e btn-e-primary" type="submit" v-on:click="findMovie" value="Search" />
-          </li>
+    <div class="contain-wrapp padding-bottom-40">
+      <div class="container">
+        <h2>Search for a Movie</h2>
+        <form class="form-inline" v-on:submit.prevent="findMovie()">
+          <div class="form-group">
+            <div class="input-group">
+              <input type="text" class="form-control" v-model="search" placeholder="Search by movie title..." />
+            </div>
+          </div>
+          <button type="submit" class="btn-e btn-e-primary" v-on:click="findMovie" value="Search">Search</button>
         </form>
-      </ul>
-      <!-- END - Gallery Filter -->
-      <div v-if="movies" class="container">
+        <div class="divider divider-2x"></div>
         <div class="row">
-          <div class="col-12">
-            <!-- START - Images Gallery -->
-            <div id="gallery" class="masonry gallery" style="position: relative; height: 1581px">
-              <div class="row">
-                <!-- START - Gallery 01 -->
-                <div
-                  data-filter="web"
-                  class="grid-item col-x12 col-md-4"
-                  style="position: absolute; left: 0px; top: 0px"
-                >
+          <!-- START - Article -->
+          <div class="col-sm-8">
+            <div v-if="movies" class="row">
+              <!-- START - Post 1 -->
+              <div v-for="movie in movies" v-bind:key="movie.imdbID" class="col-md-6">
+                <article class="post">
                   <div class="img-wrapper">
-                    <div class="img-caption ecadaZoomIn animated zoomOut">
-                      <a href="img/gallery/img01_380x380.jpg" data-pretty="prettyPhoto" class="zoomer">
-                        <i class="fa fa-search"></i>
+                    <img
+                      v-if="movie.Poster == 'N/A'"
+                      src="https://quantum-soft.net/images/75x50-1/http://s2.1pic.org/files/2019/04/16/d9c3dc1540830681331f.jpg"
+                      alt="Image not available"
+                      class="img-fluid"
+                    />
+                    <img v-else :src="movie.Poster" class="img-fluid" alt="" />
+                  </div>
+                  <div class="post-heading">
+                    <router-link class="btn-e btn-e-red btn-bordered" :to="`/movies/${movie.imdbID}`">
+                      {{ movie.Title }} ({{ movie.Year }})
+                    </router-link>
+                  </div>
+                </article>
+              </div>
+              <!-- END - Post 1 -->
+            </div>
+            <div v-else class="row">
+              <p>Hmm... I can't find that movie. Please check your spelling and try again.</p>
+            </div>
+
+            <div class="clearfix"></div>
+          </div>
+          <!-- END - Article -->
+
+          <!-- START - Aside -->
+          <div class="col-12 col-sm-4">
+            <aside>
+              <!-- START - Widget 2 -->
+              <div class="widget">
+                <h5 class="widget-head">Top Movie Articles</h5>
+                <div class="recent-widget">
+                  <div v-for="article in articles" v-bind:key="article.title" class="post">
+                    <div class="post-heading post-thumbnail">
+                      <a href="javascript:void(0);" class="post-thumbnail">
+                        <img :src="article.urlToImage" class="img-thumb" alt="" />
                       </a>
-                      <h5><a href="portfolio_single_item.html">Project Title</a></h5>
-                      <a href="javascript:void(0);" class="img-categorie">Web design</a>
-                    </div>
-                    <img src="img/gallery/img01_380x380.jpg" class="img-fluid" alt="" />
-                  </div>
-                  <div class="img-containt text-center">
-                    <h5><a href="portfolio_single_item.html">Project Title</a></h5>
-                    <a href="javascript:void(0);">Web design</a>
-                    <ul class="img-footer">
-                      <li>
-                        <i class="fa fa-download"></i>
-                        233
-                      </li>
-                      <li>
-                        <i class="fa fa-heart"></i>
-                        612
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <!-- END - Gallery 01 -->
-              </div>
-            </div>
-            <!-- END - Images Gallery -->
-          </div>
-        </div>
-      </div>
-      <div v-if="!search" class="container">
-        <div class="row">
-          <div class="col-12">
-            <!-- START - Images Gallery -->
-            <div id="gallery" class="masonry gallery" style="position: relative; height: 1770px">
-              <div class="row">
-                <!-- START - Gallery 01 -->
-                <div
-                  v-for="article in articles"
-                  v-bind:key="article.title"
-                  class="grid-item col-12"
-                  style="position: absolute; left: 0px; top: 0px"
-                >
-                  <div class="column-wrapper">
-                    <div class="img-containt half-column">
-                      <h5>{{ article.title }}</h5>
-                      <p>
-                        {{ article.description }}
-                      </p>
-                      <p>
-                        <a :href="article.url" target="_blank" class="btn-e btn-e-default btn-sm">Read more</a>
-                      </p>
-                      <ul class="img-footer">
-                        <li>
-                          {{ parsePublished(article.publishedAt) }}
-                        </li>
-                        <li>
-                          {{ article.source.name }}
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="img-wrapper half-column">
-                      <img :src="article.urlToImage" class="img-fluid" alt="" />
+                      <h6>
+                        <a :href="article.url" target="_blank">{{ article.title }}</a>
+                      </h6>
                     </div>
                   </div>
                 </div>
-                <!-- END - Gallery 01 -->
               </div>
-            </div>
-            <!-- END - Images Gallery -->
+              <!-- END - Widget 2 -->
+            </aside>
           </div>
+          <!-- END - Aside -->
         </div>
-      </div>
-    </div>
-    <h2>Search for a Movie</h2>
-    <div>
-      <form v-on:submit.prevent="findMovie()">
-        <input type="text" v-model="search" placeholder="Search by movie title..." />
-        <input type="submit" v-on:click="findMovie" value="Search" />
-      </form>
-    </div>
-
-    <!-- Movie search results -->
-    <div v-if="movies">
-      <div v-for="movie in movies" v-bind:key="movie.imdbID">
-        <router-link :to="`/movies/${movie.imdbID}`">
-          <img
-            v-if="movie.Poster == 'N/A'"
-            src="https://quantum-soft.net/images/75x50-1/http://s2.1pic.org/files/2019/04/16/d9c3dc1540830681331f.jpg"
-            alt="Image not available"
-          />
-          <img v-else :src="movie.Poster" :alt="`Poster for ${movie.title}`" />
-        </router-link>
-        <h3>{{ movie.Title }} ({{ movie.Year }})</h3>
-      </div>
-    </div>
-
-    <!-- Error handling if movie title can't be found -->
-    <div v-else><p>Hmm... I can't find that movie. Please check your spelling and try again.</p></div>
-
-    <!-- Displays the latest movies news articles if the user has not started to search for a movie -->
-    <div v-if="!search">
-      <h3>Movie News Today</h3>
-      <div v-for="article in articles" v-bind:key="article.title">
-        <a :href="article.url" target="_blank">
-          <h4>{{ article.title }}</h4>
-          <h6>Published: {{ parsePublished(article.publishedAt) }}</h6>
-          <p>Source: {{ article.source.name }} || Author: {{ article.author }}</p>
-          <img :src="article.urlToImage" alt="Article image" />
-        </a>
-
-        <p>{{ article.description }}</p>
       </div>
     </div>
   </div>
--- src/views/movies/Show.vue
+++ src/views/movies/Show.vue
@@ -1,37 +1,181 @@
 <template>
   <div class="movies-show">
-    <h2>{{ movie.Title }} ({{ movie.Year }})</h2>
-
-    <img :src="movie.Poster" alt="Movie poster" />
-    <br />
-    <button v-if="!showList" v-on:click="showFriends()">Suggest to friend</button>
-    <div v-else>
-      <label for="myFriends">Select a friend</label>
-      <select v-model="newSuggestionParams.recipient_id" name="myFriends" id="myFriends">
-        <option disabled value="">Please select one</option>
-        <option v-for="friend in friendsList" v-bind:key="friend.id" :value="friend.id">
-          {{ friend.username }}
-        </option>
-      </select>
-      <button v-on:click="createSuggestion()">Send Suggestion</button>
-      <button v-on:click="toggleFriendList()">Done</button>
+    <div class="contain-wrapp padding-bottom-clear">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <div class="heading">
+              <h4>{{ movie.Title }} ({{ movie.Year }})</h4>
+              <p>
+                {{ movie.Plot }}
+              </p>
+            </div>
+            <div
+              v-for="rating in movie.Ratings"
+              v-bind:key="rating.Value"
+              class="progress progress-primary progress-sm"
+            >
+              <div
+                class="progress-bar"
+                role="progressbar"
+                data-value-progress="60"
+                :style="`width: ${standardRating(rating.Value)}%`"
+              >
+                <span class="value-progress">
+                  {{ rating.Source }}:
+                  <strong class="countertext">{{ rating.Value }}</strong>
+                </span>
+              </div>
+            </div>
+            <button
+              v-on:click="showFriends()"
+              type="button"
+              class="btn-e btn-e-default"
+              data-toggle="modal"
+              data-target="#suggestionModal"
+            >
+              <i class="fa fa-send-o"></i>
+              Suggest to friend
+            </button>
+            <button type="button" class="btn-e btn-e-primary" data-toggle="modal" data-target=".bs-example-modal-lg">
+              <i class="fa fa-film"></i>
+              Watch a clip
+            </button>
+          </div>
+          <div class="col-12 col-md-6">
+            <img :src="movie.Poster" alt="Movie poster" class="img-fluid" />
+          </div>
+        </div>
+      </div>
     </div>
-    <br />
-    <iframe width="420" height="315" :src="movieTrailer"></iframe>
-    <br />
-    <p>Genres: {{ movie.Genre }}</p>
-    <p>Rated: {{ movie.Rated }}</p>
-    <p>Runtime: {{ movie.Runtime }}</p>
-    <p>Directed by: {{ movie.Director }}</p>
-    <p>Starring: {{ movie.Actors }}</p>
-    <p>Released: {{ movie.Released }}</p>
-    <h4>Ratings:</h4>
-    <p v-for="rating in movie.Ratings" v-bind:key="rating">Rating from {{ rating.Source }}: {{ rating.Value }}</p>
-    <p>
-      Plot:
-      <br />
-      {{ movie.Plot }}
-    </p>
+
+    <div class="modal fade" id="suggestionModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span></button>
+            <h6 class="modal-title"></h6>
+          </div>
+          <div class="modal-body">
+            <p>Select a friend to send this to:</p>
+            <select v-model="newSuggestionParams.recipient_id" class="form-control">
+              <option v-for="friend in friendsList" v-bind:key="friend.id" :value="friend.id">
+                {{ friend.username }}
+              </option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn-e btn-e-default btn-sm" data-dismiss="modal">Close</button>
+            <button v-on:click="createSuggestion()" type="button" class="btn-e btn-e-primary btn-sm">Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span></button>
+            <h6 class="modal-title">Clip</h6>
+          </div>
+          <div class="modal-body">
+            <iframe width="750" height="400" :src="movieTrailer"></iframe>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn-e btn-e-default btn-sm" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="contain-wrapp padding-bottom-50">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-12 col-md-8">
+            <div class="section-heading">
+              <h3>Facts</h3>
+
+              <i class="fa fa-film"></i>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-clock-o fa-3x"></i>
+                <h5>Run Time</h5>
+                <p>
+                  {{ movie.Runtime }}
+                </p>
+              </div>
+              <i class="fa fa-clock-o icon-bg"></i>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-drivers-license-o fa-3x"></i>
+                <h5>Rated</h5>
+                <p>
+                  {{ movie.Rated }}
+                </p>
+              </div>
+              <i class="fa fa-drivers-license-o icon-bg"></i>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-calendar fa-3x"></i>
+                <h5>Release Date</h5>
+                <p>
+                  {{ movie.Released }}
+                </p>
+              </div>
+              <i class="fa fa-calendar icon-bg"></i>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-tags fa-3x"></i>
+                <h5>Genres</h5>
+                <p>
+                  {{ movie.Genre }}
+                </p>
+              </div>
+              <i class="fa fa-tags icon-bg"></i>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-ticket fa-3x"></i>
+                <h5>Director</h5>
+                <p>
+                  {{ movie.Director }}
+                </p>
+              </div>
+              <i class="fa fa-ticket icon-bg"></i>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-md-4">
+            <div class="icon-box box-primary">
+              <div class="icon-box-contain">
+                <i class="fa fa-smile-o fa-3x"></i>
+                <h5>Actors</h5>
+                <p>
+                  {{ movie.Actors }}
+                </p>
+              </div>
+              <i class="fa fa-smile-o icon-bg"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,6 +197,7 @@ export default {
   created: function () {
     axios.get(`/movies/${this.$route.params.id}`).then((response) => {
       this.movie = response.data;
+      console.log(this.movie);
       this.getTrailer();
     });
   },
@@ -82,6 +227,16 @@ export default {
       movieTrailer(this.movie.Title, this.movie.Year).then((response) => {
         this.movieTrailer = response.replace("watch?v=", "embed/");
       });
+    },
+    standardRating: function (rating) {
+      var percent = "";
+      if (rating.includes("%")) {
+        percent = rating.replace("%", "");
+        return parseInt(percent);
+      } else if (rating.includes("/")) {
+        var rated = rating.split("/");
+        return (parseFloat(rated[0]) / parseInt(rated[1])) * 100;
+      }
     },
   },
 };
